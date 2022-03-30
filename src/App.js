170b7849ddffe0143798f67b7864d2124f2e6a6b
Tory <88531407+1two13@users.noreply.github.com>
2022-03-31 00:44:40 +0900

    features: Filter.js state 설정

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import Filter from "./components/Filter";
 
 function App() {
+	// 로딩 상태 값
+	// 공항 배열 상태 값
+
+	// useEffect 통해서 최초 로딩시 전체 목록 가져오기 /airport
+
+	const onSubmitHandler = (filter, query) => {
+		// fetch로 검색하고 공항 배열 상태 변경
+		console.log(filter, query)
+	};
+
   return (
     <div className="App">
-			<Filter />
+			<Filter onSubmitHandler={onSubmitHandler}/>
+			{/* 로딩중이냐? */}
+			{/* 배열이 비어있냐? */}
     </div> 
   );
 }
--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,21 +1,44 @@
+import { useState } from "react";
 import styled from "styled-components";
 
-const StyledFilter = styled.div`
-	.searchBtn {
-		margin-left	: 7px;
+const StyledFilter = styled.form`
+	.inputBox {
+		margin-left: 7px;
+		margin-right: 7px;
 	}
 `
 
-function Filter() {
+function Filter({onSubmitHandler}) {
+	// select 상태값
+	const [filter, setFilter] = useState();
+	// input 상태값
+	const [query, setQuery] = useState();
+
+	const filterHandler = (event) => {
+		setFilter(event.target.value)
+	}
+	const queryHandler = (event) => {
+		setQuery(event.target.value);
+	};
+
 	return (
-		<StyledFilter>
-			<select>
-				<option>공항이름</option>
-				<option>공항코드</option>
-				<option>나라이름</option>
+		<StyledFilter onSubmit={(event) => {
+			event.preventDefault();
+			onSubmitHandler(filter, query);
+		}}>
+			<select onChange={filterHandler}>
+				<option value="0">공항이름</option>
+				<option value="1">공항코드</option>
+				<option value="2">나라이름</option>
 			</select>
-			<input type="text" placeholder="공항코드 또는 공항이름을 입력해주세요." size="35"></input>
-			<button className="searchBtn">검색</button>
+			<input 
+				onChange={queryHandler}
+				className="inputBox"
+				type="text"
+				placeholder="공항코드 또는 공항이름을 입력해주세요." 
+				size="35"
+			></input>
+			<button>검색</button>
 		</StyledFilter>
 	);
 };
