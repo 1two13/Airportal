16c46e6678148204a0dcb2077a72d671b473e420
Tory <88531407+1two13@users.noreply.github.com>
2022-04-01 10:41:02 +0900

    features: CSS Loader

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,6 +1,22 @@
+import styled from "styled-components";
+
+const Loader = styled.div`
+  border: 10px solid #f3f3f3; 
+  border-top: 10px solid #22408E; 
+  border-radius: 50%;
+  width: 30px;
+  height: 30px;
+  animation: spin 2s linear infinite;
+
+@keyframes spin {
+  0% { transform: rotate(0deg); }
+  100% { transform: rotate(360deg); }
+}
+`
+
 function Loading() {
 	return(
-		<div>Hello</div>
+		<Loader></Loader>
 	)
 }
 
