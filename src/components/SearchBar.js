e1c0d422337d1d9953ad476fa38657dabc509a0e
Tory <88531407+1two13@users.noreply.github.com>
2023-05-24 01:44:05 +0900

    style: 주석 제거, 코드 위치 변경, 검색창 css 수정

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,44 +1,7 @@
-import { useState } from "react";
-import styled from "styled-components";
+import { useState } from 'react';
+import styled from 'styled-components';
 
-const Filter = styled.form`
-  display: flex;
-  justify-content: space-evenly;
-  height: 38px;
-  position: sticky;
-  top: 0px;
-  padding: 5px 0;
-  background-color: white;
-  * {
-    border: none;
-    border-radius: 5px;
-    outline-color: #baddf9;
-    color: rgb(121, 121, 121);
-    background-color: #fafafa;
-    cursor: pointer;
-  }
-`;
-
-const Input = styled.input`
-  margin-right: 7px;
-  flex-grow: 3;
-  top: 0px;
-`;
-
-const Button = styled.button`
-  flex-grow: 0.7;
-  background-color: #8aacc8;
-  color: white;
-  font-size: 15px;
-  transition: all 0.9s, color 1;
-  top: 0px;
-  &:hover {
-    background-color: #82b3c9;
-  }
-`;
-
-function SearchBar({ onSubmitHandler, initialQuery = "" }) {
-  // input 상태값
+function SearchBar({ onSubmitHandler, initialQuery = '' }) {
   const [query, setQuery] = useState(initialQuery);
   const queryHandler = (event) => {
     setQuery(event.target.value);
@@ -64,3 +27,40 @@ function SearchBar({ onSubmitHandler, initialQuery = "" }) {
 }
 
 export default SearchBar;
+
+const Filter = styled.form`
+  display: flex;
+  justify-content: space-evenly;
+  height: 38px;
+  position: sticky;
+  top: 0px;
+  padding: 5px 0;
+  background-color: white;
+  * {
+    border: none;
+    border-radius: 5px;
+    outline-color: #baddf9;
+    color: rgb(121, 121, 121);
+    background-color: #fafafa;
+    cursor: pointer;
+  }
+`;
+
+const Input = styled.input`
+  margin-right: 7px;
+  flex-grow: 3;
+  top: 0px;
+  padding-left: 1%;
+`;
+
+const Button = styled.button`
+  flex-grow: 0.7;
+  background-color: #8aacc8;
+  color: white;
+  font-size: 15px;
+  transition: all 0.9s, color 1;
+  top: 0px;
+  &:hover {
+    background-color: #82b3c9;
+  }
+`;
