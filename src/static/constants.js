07fa85c71c20a8ded2495707f746773ebb63a7b2
Tory <88531407+1two13@users.noreply.github.com>
2023-05-24 13:39:15 +0900

    feat: 상수 파일 생성

---
+++ src/static/constants.js
@@ -0,0 +1,8 @@
+const ALL = '전체';
+const IATA = '공항코드';
+const COUNTRY = '나라';
+const LOCATION = '위치';
+const AIRPORT_NAME = '공항명';
+const SEARCHBAR_PLACEHOLDER = '공항코드 / 나라 / 위치 / 공항명 을(를) 입력해주세요.';
+
+export { ALL, IATA, COUNTRY, LOCATION, AIRPORT_NAME, SEARCHBAR_PLACEHOLDER };
